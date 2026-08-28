fc49ea94b8b206a17d951b3139293c1ad2e50981
Husen Khaddour <husenkhaddour@gmail.com>
2026-08-28 14:50:41 +0300

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,13 +3,12 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     return const FirebaseOptions(
-      apiKey: "AIzaSyA1SCOZ4fFlNVme4Tog6iGJLML92fkzL1A",
+      apiKey: "AIzaSyAMf7MqLMAlerKDxcJn-pgMLjXRRJhYLvI",
       authDomain: "eco-alert-ff81f.firebaseapp.com",
       projectId: "eco-alert-ff81f",
       storageBucket: "eco-alert-ff81f.firebasestorage.app",
-      messagingSenderId: "831490656695", 
-      appId: "1:831490656695:android:f8e9e9cf8e90d103c84032", 
-      measurementId: "G-5N5HQ6BBZC",
+      messagingSenderId: "831490656695",
+      appId: "1:831490656695:android:f8e9e9cf8e90d103c84032",
     );
   }
 }
